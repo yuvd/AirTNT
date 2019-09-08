8f128aefc4dd0267a85befb7698c3aef3be8a47d
Yuval Drori <yuvald29@protonmail.com>
2019-09-08 16:48:05 +0300

    finish routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources: units, only: []
+  resources :units
   devise_for :users
-  root to: 'pages#home'
+  root to: 'units#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
