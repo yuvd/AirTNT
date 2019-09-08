496aa302c85b161de0bdfa9db4136bb5695f04d8
Yuval Drori <yuvald29@protonmail.com>
2019-09-08 17:55:20 +0300

    create router actions for index new and create + _form + form for new

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -1,10 +1,28 @@
 class UnitsController < ApplicationController
+  #before_action :authenticate_user!
+  #skip_before_action :authenticate_user!, only: :index, :new
+
   def index
+    @units = Unit.all
   end
-
+  
   def new
+    @unit = Unit.new
   end
-
+  
   def create
+    @unit = Unit.new(unit_params)
+    @unit.user = current_user
+    if @unit.valid?
+      @unit.save
+      redirect_to '/'
+    else
+      
+      render :new
+    end
+  end
+  
+  def unit_params
+    params.require(:unit).permit(:name, :user, :photo, :category, :description)
   end
 end
