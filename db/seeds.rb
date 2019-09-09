fc78562b185ca808f1ec7452f49f4239d1f0e794
Robert Lenckowski <rlenckowski@gmail.com>
2019-09-09 17:43:23 +0300

    index prepare

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Unit.create(name: "Blabla", photo: "https://picsum.photos/200/300", category: "Kidnapping", description: "blsfvsfv sdvs sds sdsv sdvsv sdvsv")
+# Unit.create(name: "Jivv", photo: "https://picsum.photos/200/300", category: "Kidnapping", description: "blsfvsfv sdvs sds sdsv sdvsv sdvsv")
+# Unit.create(name: "Ijdsv", photo: "https://picsum.photos/200/300", category: "Kidnapping", description: "blsfvsfv sdvs sds sdsv sdvsv sdvsv")
+# Unit.create(name: "On sdv", photo: "https://picsum.photos/200/300", category: "Kidnapping", description: "blsfvsfv sdvs sds sdsv sdvsv sdvsv")
+# Unit.create(name: "Ojsbsvd", photo: "https://picsum.photos/200/300", category: "Kidnapping", description: "blsfvsfv sdvs sds sdsv sdvsv sdvsv")
+# Unit.create(name: "Njsd", photo: "https://picsum.photos/200/300", category: "Kidnapping", description: "blsfvsfv sdvs sds sdsv sdvsv sdvsv")
+# Unit.create(name: "Jwdv", photo: "https://picsum.photos/200/300", category: "Kidnapping", description: "blsfvsfv sdvs sds sdsv sdvsv sdvsv")
+# Unit.create(name: "Wdsc", photo: "https://picsum.photos/200/300", category: "Kidnapping", description: "blsfvsfv sdvs sds sdsv sdvsv sdvsv")
+# Unit.create(name: "Isdv", photo: "https://picsum.photos/200/300", category: "Kidnapping", description: "blsfvsfv sdvs sds sdsv sdvsv sdvsv")
+# Unit.create(name: "Bsdv", photo: "https://picsum.photos/200/300", category: "Kidnapping", description: "blsfvsfv sdvs sds sdsv sdvsv sdvsv")
+
+
+
+
+
+Unit.destroy_all
+User.destroy_all
+user = User.create!(email: 'jpeterek@google.com', password: 'testtest')
+p user
+6.times do
+Unit.create!(name: 'unit awesome' ,category: 'Financial Problems', description: 'Specialized on solving financial disputes', user_id: user.id, photo:'https://lp-cms-production.imgix.net/news/2017/08/bodyguard.jpg?fit=max&w=800&q=40&sharp=10&vib=20')
+end
