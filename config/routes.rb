b619f1800a75020d1a8697cf2b0ac518db1261d1
Janina Peterek <jpeterek@google.com>
2019-09-11 14:34:30 +0300

    done with booking layout and editing

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   root to: 'units#index'
   get 'profile', to: 'dashboards#profile'
   resources :units do
-    resources :bookings, only: [:new, :create, :edit, :update]
+    resources :bookings, only: [:new, :create]
   end
-  resources :bookings, only: [:index, :destroy]
+  resources :bookings, only: [:index, :destroy, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
